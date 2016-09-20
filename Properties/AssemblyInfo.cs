3cde8c0358abae47dafefc284395ff15878d5813
jensborn <jens.bornschein@tu-dresden.de>
2016-09-20 09:20:20 +0200

    add infos to the assembly

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LanguageLocalization")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("TU Dresden, Germany")]
 [assembly: AssemblyProduct("LanguageLocalization")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
